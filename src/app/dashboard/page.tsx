6a0d68b89d4cc2f68c377609511cf62f4daa665d
dev.rajdeep <deepbn9@gmail.com>
2024-05-06 18:16:12 +0530

    feat: added card element on the dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,36 @@
-import { Container, Grid, List, ListItem, ListItemText } from "@mui/material";
+import { Container, List, ListItem, ListItemText, Card } from "@mui/material";
 
 export default function Home() {
   return (
-    <Container component="main">
-      <Grid container justifyContent="center">
-        <Grid item xs="auto">
-          <List>
-            {Array.from({ length: 10 }).map((_, index) => (
-              <ListItem key={index}>
-                <ListItemText
-                  primary={`Item ${index + 1}`}
-                  secondary="Secondary text"
-                />
-              </ListItem>
-            ))}
-          </List>
-        </Grid>
-      </Grid>
+    <Container
+      component="main"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <Card
+        sx={{
+          minWidth: 200,
+          maxHeight: 500,
+          height: "100%",
+          overflow: "hidden",
+        }}
+      >
+        <List sx={{ overflowY: "auto", height: "100%" }}>
+          {Array.from({ length: 10 }).map((_, index) => (
+            <ListItem key={index}>
+              <ListItemText
+                primary={`Item ${index + 1}`}
+                secondary="Secondary text"
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Card>
     </Container>
   );
 }
